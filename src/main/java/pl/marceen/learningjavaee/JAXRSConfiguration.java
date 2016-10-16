06eaa22219f363f787bff61fa5b38c15a7d4a360
Marcin Zaremba <marcinzaremba@gmail.com>
2016-10-16 23:00:58 +0200

    Build Hello World!

---
+++ src/main/java/pl/marceen/learningjavaee/JAXRSConfiguration.java
@@ -0,0 +1,7 @@
+package pl.marceen.learningjavaee;
+
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("api/rest")
+public class JAXRSConfiguration {
+}
---
+++ src/main/java/pl/marceen/learningjavaee/helloworld/boundary/HelloWorldResource.java
@@ -0,0 +1,24 @@
+package pl.marceen.learningjavaee.helloworld.boundary;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Stateless
+@Path("helloWorld")
+public class HelloWorldResource {
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldResource.class);
+
+    @GET
+    @Path("currentTime")
+    public String currentTime() {
+        String msg = String.format("Current time: %s", System.currentTimeMillis());
+
+        logger.info(msg);
+        return msg;
+    }
+
+}
