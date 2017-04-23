41978ee87ec9246590969c6fd1155708723581c1
Marcin Zaremba <marcin.zaremba@bm.pl>
2017-04-23 13:44:57 +0200

    Volatile modifier test

---
+++ LearningJavaEE/src/test/java/pl/marceen/learningjavaee/modifiers/VolatileTest.java
@@ -0,0 +1,47 @@
+package pl.marceen.learningjavaee.modifiers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Marcin Zaremba
+ */
+public class VolatileTest {
+    private final static Logger logger = LoggerFactory.getLogger(VolatileTest.class);
+
+    private static volatile int MY_INT = 0;
+
+    public static void main(String[] args) {
+        new ChangeListener().start();
+        new ChangeMaker().start();
+    }
+
+    static class ChangeListener extends Thread {
+        @Override
+        public void run() {
+            int local_value = MY_INT;
+            while (local_value < 5) {
+                if (local_value != MY_INT) {
+                    logger.info("Got Change for MY_INT : {}", MY_INT);
+                    local_value = MY_INT;
+                }
+            }
+        }
+    }
+
+    static class ChangeMaker extends Thread {
+        @Override
+        public void run() {
+            int local_value = MY_INT;
+            while (MY_INT < 5) {
+                logger.info("Incrementing MY_INT to {}", local_value + 1);
+                MY_INT = ++local_value;
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
